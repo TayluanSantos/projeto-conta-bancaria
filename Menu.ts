fd38c368098cebd9bab644c8ee9e5dcad258ab23
TayluanSantos <tayluanjesus@gmail.com>
2024-07-03 12:16:45 -0300

    feat: Adicionando a classe Conta e incrementando a classe Menu

--- Menu.ts
+++ Menu.ts
@@ -1,9 +1,45 @@
 import readlinesync = require("readline-sync");
 import { colors } from "./src/util/Colors";
+import { Conta } from "./src/model/Conta";
 
 export function main() {
 
     let opcao: number;
+
+    //Novas Instâncias da Classe Conta (Objetos)
+    const c1: Conta = new Conta(1, 1234, 1, 'Júlia Castro', 800000.00);
+    const c2: Conta = new Conta(2, 1234, 2, 'Marcella Sanches', 600000.00);
+
+    // Visualizando os dados da Conta c1
+    c1.visualizar();
+
+    // Visualizando os dados da Conta c2
+    c2.visualizar();
+
+    // Visualizando o Saldo da Conta c1
+    console.log(`\nO Saldo da conta 01 é: ${c1.saldo}`);
+
+    // Alterando o Saldo da Conta c2
+    c2.saldo = 900000.00;
+
+    // Visualizando o Saldo da Conta c2, depois de atualizar o valor
+    console.log(`\nO Saldo da conta 02 é: ${c2.saldo}`);
+
+    // Saque nas Contas
+    console.log(`\nSacar 100.00 Reais da Conta C1: ${c1.sacar(100)}`); // true
+    c1.visualizar();
+
+    console.log(`\nSacar 1000000.00 Reais da Conta C2: ${c2.sacar(1000000)}`); // false
+    c2.visualizar();
+
+    // Depósito nas Contas
+    console.log(`\nDepositar 200000.00 Reais da Conta C1: `); 
+    c1.depositar(200000)
+    c1.visualizar();
+
+    console.log(`\nDepositar 300000.25 Reais da Conta C2: `); 
+    c2.depositar(300000.25)
+    c2.visualizar();
 
     while (true) {
         console.log(colors.fg.magentastrong)
@@ -44,14 +80,17 @@ export function main() {
 
                 break;
             case 2:
+                console.log(colors.fg.magentastrong)
                 console.log("\n\nListar todas as Contas\n\n");
 
                 break;
             case 3:
+                console.log(colors.fg.magentastrong)
                 console.log("\n\nConsultar dados da Conta - por número\n\n");
 
                 break;
             case 4:
+                console.log(colors.fg.magentastrong)
                 console.log("\n\nAtualizar dados da Conta\n\n");
 
                 break;
@@ -87,7 +126,7 @@ export function sobre(): void {
     console.log("\n*****************************************************");
     console.log("Projeto Desenvolvido por: Tayluan de Jesus dos Santos");
     console.log("Generation Brasil - generation@generation.org");
-    console.log("github.com/conteudoGeneration");
+    console.log("github.com/TayluanSantos");
     console.log("*****************************************************");
 }
 
