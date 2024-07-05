a81f30398728ea654223ab0e903e362885e57971
TayluanSantos <tayluanjesus@gmail.com>
2024-07-05 10:29:27 -0300

    feat: Adicionando a classe ContaRepository

---
+++ src/repository/ContaRepository.ts
@@ -0,0 +1,23 @@
+import { Conta } from "../model/Conta";
+
+export interface ContaRepository {
+
+    // Métodos CRUD
+
+    procurarPorNumero(numero : number) : void;
+    listaTodas() : void;
+    cadastrar(conta : Conta) : void;
+    atualizar(conta : Conta) : void;
+    deletar (numero : number) : void;
+
+    // Métodos Bancários 
+
+    sacar(numero: number , valor: number) : void;
+    depositar(numero: number , valor: number) : void;
+    transferir(numeroOrigem: number, numeroDestino: number , valor: number) : void;
+
+    
+
+
+    
+}
